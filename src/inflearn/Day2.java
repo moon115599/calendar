28cac52863bd0097563be4a435380a9a3bcfe6b1
문희석 <@DESKTOP-HCQAGGT>
2020-12-18 15:07:47 +0900

    sum

---
+++ src/inflearn/Day2.java
@@ -0,0 +1,15 @@
+package inflearn;
+import java.util.Scanner;
+public class Day2 {
+
+	public static void main(String[] args) {
+	Scanner scr = new Scanner(System.in);
+	System.out.println("두 수를 입력하세요");
+	int a = scr.nextInt();
+	int b = scr.nextInt();
+	System.out.println("두 수의 합은 "+(a+b)+"입니다.");
+		
+
+	}
+
+}
