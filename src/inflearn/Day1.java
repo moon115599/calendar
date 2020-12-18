661731e8c0e0e5f681990276f85d1d794a3abb33
문희석 <@DESKTOP-HCQAGGT>
2020-12-18 14:45:22 +0900

    update calendar main()

--- src/inflearn/Day1.java
+++ src/inflearn/Day1.java
@@ -3,7 +3,11 @@ package inflearn;
 public class Day1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Calendar");
+		System.out.println(" 일   월   화   수   목   금   토");
+		System.out.println("--------------------");
+		System.out.println(" 1  2  3  4  5  6  7");
+		System.out.println(" 8  9 10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
 		
 
 	}
